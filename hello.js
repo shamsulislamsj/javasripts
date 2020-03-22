f7f21914aeb5bd068359cfedd3f3027a0ef1316f
juwelahmedsj <shamsulislam5333@gmail.com>
2020-03-22 20:21:11 +0600

    javasripts rules

---
+++ hello.js
@@ -0,0 +1,12 @@
+console.log(21);
+console.log(121);
+//VARIABLE :
+//KIND OF VARIABLE: NUMERIC , STRING , BOOLEAN
+//1.NUMERIC:
+var bananaPrice = 12;
+console.log(bananaPrice);
+console.log(typeof bananaPrice);
+
+var seenAfter = 21;
+console.log(seenAfter);
+console.log(typeof seenAfter);
