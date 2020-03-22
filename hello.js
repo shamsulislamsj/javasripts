014d43a554ba47ce3e1a913b235d0d18572d90f1
juwelahmedsj <shamsulislam5333@gmail.com>
2020-03-22 20:44:31 +0600

    first bootstrap

--- hello.js
+++ hello.js
@@ -10,3 +10,23 @@ console.log(typeof bananaPrice);
 var seenAfter = 21;
 console.log(seenAfter);
 console.log(typeof seenAfter);
+//2.STRING VARIABLE:
+var name = "kolimUddin Zukerbarg"
+console.log(name);
+console.log(typeof name);
+
+var name = "salmanKhan";
+console.log(name);
+console.log(typeof name);
+
+//3.BOOLEAN VARIABLE
+var isHot = true;
+var isRich = false;
+console.log(isHot);
+console.log(typeof isHot);
+
+//JAVASCRIPTS PROGRAMMING KEW WORD 
+//abstract	arguments	boolean	break byte	case	catch	chaconst	continue	debugger	default delete	do	double	else eval	false	final	finally float	for	function	goto
+//if	     implements	in	instanceof int   	interface	let	long native	new	null	package private	protected	public	return short	static	switch	synchronized
+//this throw	throws	transient true	try	typeof	var void	volatile	while	with yield
+// NEW ADD KEYWORD : class	enum	export	extends import	super
